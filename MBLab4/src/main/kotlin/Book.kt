4f95a0b373c6139cb81ef88aa8af7f3d88d739a0
Hritu Toma <96055108+HrituToma@users.noreply.github.com>
2022-10-27 13:46:05 +0300

    Lab 4

---
+++ MBLab4/src/main/kotlin/Book.kt
@@ -0,0 +1,22 @@
+class Book(Booktitle: String) : Section(Booktitle) {
+
+    val bt = Booktitle
+
+    val authors = mutableListOf<Author>()
+
+    fun addAuthor(author: Author) {
+        authors.add(author)
+    }
+
+    fun addContent(element: Element) {
+        super.add(element)
+    }
+
+    override fun print() {
+        for(a in authors) {
+            println("Authors:")
+            a.print()
+        }
+        super.print()
+    }
+}
